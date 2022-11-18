d93f9eee053336edc1aad2ffc74b7c56788c7c16
Navyadandamudi12 <105265235+Navyadandamudi12@users.noreply.github.com>
2022-11-18 12:39:56 +0530

    Add files via upload

---
+++ Account-MS/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000001,'CUSTOMER101',10000.00, 'Savings',TO_DATE('09/01/2022', 'DD/MM/YYYY'), 'Navya');
+
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000002, 'CUSTOMER102', 20000.00, 'Current', TO_DATE('09/01/2022', 'DD/MM/YYYY'),'Sri');
