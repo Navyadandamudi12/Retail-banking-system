8ad21da5cfe1335fcd723e48bf7bb1abb837e5f3
Navyadandamudi12 <105265235+Navyadandamudi12@users.noreply.github.com>
2022-11-23 21:01:31 +0530

    Add files via upload

---
+++ Authentication-MS/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into appuser (userid,username,password,role) values ('EMPLOYEE101','ns','nav','EMPLOYEE');
+insert into appuser (userid,username,password,role) values ('CUSTOMER101','customer','customer','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER102','customer','customer','CUSTOMER');
