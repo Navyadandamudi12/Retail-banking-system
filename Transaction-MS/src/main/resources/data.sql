e939a62fe7fde6c3f2d090d75608c82368ddf80a
Navyadandamudi12 <105265235+Navyadandamudi12@users.noreply.github.com>
2022-11-23 21:05:56 +0530

    Add files via upload

---
+++ Transaction-MS/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TRANSACTION(id, source_Account_Id,source_Owner_Name,target_Account_Id,target_Owner_Name,amount,initiation_Date,reference)
+VALUES (1, 1000000001,'Navya', 1000000002, 'Sri', 100.00, '2022-01-09 10:30', 'transfer');
+
